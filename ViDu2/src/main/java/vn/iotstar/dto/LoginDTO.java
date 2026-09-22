2134f524d8d85aaa93d4d20ddfcee47cbfee316f
Vo Van Thinh <vovanthinh.gmail.com>
2026-09-22 09:35:01 +0700

    Ví dụ 2: Tạo DTO UserDTO, LoginDTO và MapStruct UserMapper

---
+++ ViDu2/src/main/java/vn/iotstar/dto/LoginDTO.java
@@ -0,0 +1,19 @@
+package vn.iotstar.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginDTO {
+    @NotBlank(message = "Username hoặc email không được để trống")
+    private String login;
+
+    @NotBlank(message = "Password không được để trống")
+    private String password;
+
+    public LoginDTO() {}
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
